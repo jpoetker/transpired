6cc71161743f5d7e6ce2dec4e32e11f438ff89be
Jeff Poetker <jpoetker@dotloop.com>
2015-02-17 12:08:53 -0500

    Further initializing the project

---
+++ transpired-app/src/main/java/jpoetker/transpired/Application.java
@@ -0,0 +1,16 @@
+package jpoetker.transpired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="jpoetker.transpired")
+@EnableAutoConfiguration
+public class Application {
+	
+	public static void main(String... args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
